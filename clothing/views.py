3691e5808030c2ad451ad80f559ed701fa7a3a73
Luis Gutierrez <luismanugutierrez@gmail.com>
2015-10-01 02:20:22 -0700

    small changes

--- clothing/views.py
+++ clothing/views.py
@@ -276,6 +276,8 @@ def about(request):
 @csrf_protect
 def testing(request):
 	'''
+	THIS CODE HAS BEEN MOVED TO custom_registration.
+
 	Creating a User, UserProfile, authenticating, and logging in. Works! Sweet!
 
 	Used django.contrib.auth.forms import UserCreationForm in UserCreateForm in forms.py.
@@ -373,65 +375,15 @@ def like_product(request):
 
 
 
-
-
-
-
-
-
-
-
-# def register(request):
-
-# 	registered = False
-
-# 	if request.method == 'POST':
-# 		user_form = UserForm(data=request.POST)
-# 		profile_form = UserProfileForm(data=request.POST)
-# 		if user_form.is_valid() and profile_form.is_valid():
-# 			user = user_form.save()
-# 			user.set_password(user.password)
-# 			user.save()
-# 			profile = profile_form.save(commit=False)
-# 			profile.user = user
-# 			if 'picture' in request.FILES:
-# 				profile.picture = request.FILES['picture']
-# 			profile.save()
-# 			registered = True
-# 		else:
-# 			print user_form.errors, profile_form.errors
-
-# 	else:
-# 		user_form = UserForm()
-# 		profile_form = UserProfileForm()
-# 	return render(request, 'rango/register.html',
-# 		{'user_form': user_form, 'profile_form': profile_form, 'registed': registered})
-
-
-# def user_login(request):
-# 	if request.method == 'POST':
-# 		username = request.POST.get('username')
-# 		password = request.POST.get('password')
-# 		user = authenticate(username=username, password=password)
-# 		if user:
-# 			if user.is_active:
-# 				login(request, user)
-# 				return HttpResponseRedirect('/rango/')
-# 			else:
-# 				return HttpResponse("Your Rango account is disabled.")
-# 		else:
-# 			print "Invalid login details: {0}, {1}".format(username, password)
-# 			return HttpResponse("Invalid login details supplied.")
-# 	else:
-# 		return render(request, 'rango/login.html', {})
-
-
-
 from registration.backends.simple.views import RegistrationView
 
 # from clothing.models import UserProfile
 
 class MyRegistrationView(RegistrationView):
+	'''
+	This has been depricated. Works. Can be used with Django-registration-redux or UserCreateForm(UserCreationForm):
+
+	'''
 	def get_success_url(self,request, user):
 		user = request.user
 		userprofile = UserProfile(user=user)
